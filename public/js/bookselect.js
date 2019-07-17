8334dc2fb6cb8af48552fd9cfc3fb71bf18519a0
CaptainEFFF <davidbanville4@gmail.com>
2019-07-16 21:18:46 -0400

    bookshelf page

---
+++ public/js/bookselect.js
@@ -0,0 +1,105 @@
+var Games = (function() {
+
+	var transEndEventNames = {
+			'WebkitTransition' : 'webkitTransitionEnd',
+			'MozTransition' : 'transitionend',
+			'OTransition' : 'oTransitionEnd',
+			'msTransition' : 'MSTransitionEnd',
+			'transition' : 'transitionend'
+		}, 
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		$games = $( '#bk-list > li > div.bk-game' ), gamesCount = 13, currentgame = -1,counter = 0; 
+	
+	function init() {
+
+		$games.each( function( i ) {
+			
+			var $game = $( this ),
+				$other = $games.not( $game ),
+				$parent = $game.parent(),
+				$page = $game.children( 'div.bk-page' ),
+				$content = $page.children( 'div.bk-content' ), current = 0;
+			
+			if (counter === 13){
+				counter = 0;
+			}
+
+			if( counter < gamesCount / 2 ) {
+				$parent.css( 'z-index', counter ).data( 'stackval', counter );
+				counter ++;
+			
+			}
+			else {
+				$parent.css( 'z-index', gamesCount - 1 - counter ).data( 'stackval', gamesCount - 1 - counter );
+				counter ++;	
+			}
+
+			$game.on( 'click', function() {
+				
+				if( currentgame !== -1 && currentgame !== $parent.index() ) {
+					closeCurrent();
+				}
+				
+				if( $game.data( 'opened' ) ) {
+					$game.data( 'opened', false ).removeClass( 'bk-viewinside' ).on( transEndEventName, function() {
+						$( this ).off( transEndEventName ).removeClass( 'bk-outside' );
+						$parent.css( 'z-index', $parent.data( 'stackval' ) );
+						currentgame = -1;
+					} );
+				}
+				else {
+					$game.data( 'opened', true ).addClass( 'bk-outside' ).on( transEndEventName, function() {
+						$( this ).off( transEndEventName ).addClass( 'bk-viewinside' );  
+						$parent.css( 'z-index', gamesCount );
+						currentgame = $parent.index();
+					} );
+					current = 0;
+					$content.removeClass( 'bk-content-current' ).eq( current ).addClass( 'bk-content-current' );
+				}
+
+			} );
+
+			if( $content.length > 1 ) {
+
+				var $navPrev = $( '<span class="bk-page-prev">&lt;</span>' ),
+					$navNext = $( '<span class="bk-page-next">&gt;</span>' );
+				
+				$page.append( $( '<nav></nav>' ).append( $navPrev, $navNext ) );
+
+				$navPrev.on( 'click', function() {
+					if( current > 0 ) {
+						--current;
+						$content.removeClass( 'bk-content-current' ).eq( current ).addClass( 'bk-content-current' );
+					}
+					return false;
+				} );
+
+				$navNext.on( 'click', function() {
+					if( current < $content.length - 1 ) {
+						++current;
+						$content.removeClass( 'bk-content-current' ).eq( current ).addClass( 'bk-content-current' );
+					}
+					return false;
+				} );
+
+			}
+			
+		} );
+
+	}
+
+	function closeCurrent() {
+
+		var $game = $games.eq( currentgame ),
+			$parent = $game.parent();
+		
+		$game.data( 'opened', false ).removeClass( 'bk-viewinside' ).on( transEndEventName, function(e) {
+			$( this ).off( transEndEventName ).removeClass( 'bk-outside' );
+			$parent.css( 'z-index', $parent.data( 'stackval' ) );
+		} );
+
+	}
+
+	return { init : init };
+
+})();
---
+++ public/js/bookshelf.js
@@ -0,0 +1,499 @@
+
+// replace with reference to database.
+var games = [
+{Title : "Borderlands",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Bioshock",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "The Legend of Zelda",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Sonic Adventure",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Super Mario Bros.",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Minecraft",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Dark Souls",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Halo",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Skyrim",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Borderlands",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Bioshock",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "The Legend of Zelda",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Sonic Adventure",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Super Mario Bros.",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Minecraft",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Dark Souls",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Halo",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Skyrim",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Borderlands",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Bioshock",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "The Legend of Zelda",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Sonic Adventure",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Super Mario Bros.",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Minecraft",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Dark Souls",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Halo",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Skyrim",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Borderlands",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Bioshock",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "The Legend of Zelda",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Sonic Adventure",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Super Mario Bros.",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Minecraft",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Dark Souls",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Halo",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+{Title : "Skyrim",
+System_type: "PS4",
+Year_released: 2012,
+is_Physical: true,
+is_beaten: true,
+personal_rating: 10,
+notes:"",
+api_url:"",
+giant_bomb_ID:"",
+box_art:"",
+description:""
+},
+
+]
+
+var game;
+var front;
+var cover;
+var page;
+var back;
+var right;
+var left;
+var topa;
+var bottom;
+var counter;
+
+function creategame(){
+game;
+front;
+cover;
+page;
+back;
+right;
+left;
+topa;
+bottom;
+counter = 0;
+
+
+for (var i = 0; i < games.length; i++){
+    game = $('<li class =' + i + '> <div class="bk-game game' + i + '"></div></li>' )
+    $("#bk-list").append(game);
+
+    front = $(`<div class="bk-front"></div>`)
+    
+    $(front).append(`<div class="bk-cover-back"></div>`);
+    
+    cover = $(`<div class="bk-cover">`)
+    $(cover).append('<h2> <span>' + games[i].Title + '</span><span>'+ games[i].System_type +'</span></h2></div></div>');
+    $(front).append(cover);
+    
+    page = $(`<div class="bk-page">`)
+    $(page).append('<div class="bk-content bk-content-current"><p>' + games[i].Description + '</p></div>')
+    $(page).append('<div class="bk-content "><p>' + games[i].is_Physical + " " + games[i].is_beaten + " " + games[i].personal_rating + '</p></div>')
+    $(page).append('<div class="bk-content "><p>' + games[i].Notes + '</p></div></div>')
+    
+    back = $(`<div class="bk-back">`)
+    $(back).append('</p>' + games[i].Title + " " + games[i].Description +" "+ games[i].giant_bomb_ID + '</p></div>')
+
+    right = (`<div class="bk-right"></div>`);
+    left = (`<div class="bk-left"><h2><span>` + games[i].Title + '  </span><span>'+ games[i].System_type +`</span></h2>`);
+    topa = (`<div class="bk-top"></div>`);
+    bottom = (`<div class="bk-bottom"></div>`);
+
+    $(".game"+i).append(front,page,back,right,left,topa,bottom);
+   counter ++;
+   if (counter === 13){
+    $("#bk-list").append("<div class=shelf></div>");
+       counter = 0;
+   }
+}
+}
+
+creategame();
+
+
