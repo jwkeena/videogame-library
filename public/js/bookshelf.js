6a5f927e8d7db7d622e233123984ffbdacde8660
CaptainEFFF <davidbanville4@gmail.com>
2019-07-19 00:16:53 -0400

    async suxss

--- public/js/bookshelf.js
+++ public/js/bookshelf.js
@@ -373,11 +373,11 @@ for (var i = 0; i < games.length; i++){
     $("#bk-list").append("<div class=shelf></div>");
        counter = 0;
    }
-}
-if(i===(games.length-1)){
-$.getScript("bookselect.js",function(){
-    Dames.init()
-    });
+   if(i===(games.length-1)){
+   $.getScript("bookselect.js",function(){
+       Dames.init()
+       });
+   }
 }
 
     
