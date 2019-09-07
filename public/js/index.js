bc634cf72a81198b563ad59c9e364c66bf089d82
jwkeena <jwkeena@gmail.com>
2019-09-07 14:02:34 -0400

    Update packages, remove vulnerability

--- public/js/index.js
+++ public/js/index.js
@@ -276,7 +276,6 @@ function isNumber(evt) {
   var iKeyCode = (evt.which) ? evt.which : evt.keyCode
   if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
     return false;
-
   return true;
 };
 
